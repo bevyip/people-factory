4509b8d9195de5ccdf87f8712abc4648ad04a5b3
hyip8000 <hyip@purdue.edu>
2017-06-05 14:08:37 -0400

    Did first two parts of homework

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const personForm = document.querySelector('#personForm')
+
 function handleSubmit(e){
     e.preventDefault()
     const f = e.target
     const name = f.personName.value
-    document.querySelector('h1').textContent = name
+    const lastName = f.personLastName.value
+    document.querySelector('h1').textContent = "This page belongs to " + name + " " + lastName
+    document.querySelector('#empty').textContent = "Welcome, " + name + " " + lastName
 }
 personForm.addEventListener("submit",handleSubmit)
