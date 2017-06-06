261d9e807bb7ea913724cc9a4686911b0c29d9c1
hyip8000 <hyip@purdue.edu>
2017-06-06 11:38:54 -0400

    Extract list building into more functions

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 const personForm = document.querySelector('#personForm')
 
-function handleSubmit(e){
-    e.preventDefault()
-    const f = e.target
-    const name = f.personName.value
-    const lastName = f.personLastName.value 
-    const col = f.col.value
-    const details = document.querySelector('#details')
-    //details.innerHTML = `<em>${name}</em>` //diff from .textContent - legit change design
-    //backticks (`) and ${} to put expression inside no need + 
-    const favColor = f.favoriteColor.value
-    const age = f.age.value
-
-    // const colorDiv = `
-    //     <div style="background-color: ${favColor}; width: 100px; height: 50px;"</div>
-    // `
-    
-    function renderColor(color){
+function renderColor(color){
         const colorDiv = document.createElement('div')
         colorDiv.style.backgroundColor = color
         colorDiv.style.width = '100px'
@@ -25,13 +9,53 @@ function handleSubmit(e){
         return colorDiv
     }
     
-    details.innerHTML = `
-        <ul>
-            <li>Name: ${name}</li>
-            <li>Favourite Colour: ${renderColor(favColor).outerHTML}</li>
-            <li>Age: ${age}</li>
-        </ul>
-    `
+function renderListItem(fieldName, value){
+        const li = document.createElement('li')
+        li.innerHTML = `${fieldName}: ${value}`
+        
+        return li
+    }
+
+function renderList(personData){
+        const list = document.createElement('ul')
+        
+        //loop over ['name, 'favColor', 'age']
+        Object.keys(personData).map(function(fieldName){
+            const li = renderListItem(fieldName, personData[fieldName])
+            list.appendChild(li)
+        })
+        
+        return list 
+    }
+    
+
+function handleSubmit(e){
+    e.preventDefault()
+    const f = e.target
+    const lastName = f.personLastName.value 
+    const col = f.col.value
+    const details = document.querySelector('#details')
+    //details.innerHTML = `<em>${name}</em>` //diff from .textContent - legit change design
+    //backticks (`) and ${} to put expression inside no need + 
+    
+    const person = {
+        name: f.personName.value,
+        favColor: renderColor(f.favoriteColor.value).outerHTML,
+        age: f.age.value,
+    }
+
+    details.appendChild(renderList(person))
+    // const colorDiv = `
+    //     <div style="background-color: ${favColor}; width: 100px; height: 50px;"</div>
+    // `
+
+//    details.innerHTML = `
+//         <ul>
+//             <li>Name: ${name}</li>
+//             <li>Favourite Colour: ${renderColor(favColor).outerHTML}</li>
+//             <li>Age: ${age}</li>
+//         </ul>
+//     `
 
     // const em = document.createElement('em')
     // em.textContent = name 
